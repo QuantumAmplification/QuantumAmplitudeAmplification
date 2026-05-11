f9fcb0b486b974704c5d9d8e01ccfa4ea8087d0e
varun29-git <vjdaiya29@gmail.com>
2026-05-11 23:00:33 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/QuantumAmplitudeAmplification',
+  trailingSlash: true,
   turbopack: {
     root: __dirname,
   },
